34635583789b7b353b3c81c371dfb6df4543022a
rodrigo@crowdbotics.com <rodrigo@crowdbotics.com>
2022-10-05 17:13:04 -0300

    Crowdbotics Studio: Update app screens

--- screens/Untitled1Copy/index.js
+++ screens/Untitled1Copy/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
 
-const Untitled2 = () => {
-  return <View style={_styles.iQYDmFzV}>
+const Untitled1Copy = () => {
+  return <View style={_styles.BLSQpQFL} __uuid__="5bc56d2c-13ec-4bbe-87a3-0e28dee3fbf8">
       </View>;
 };
 
-export default Untitled2;
+export default Untitled1Copy;
 
 const _styles = StyleSheet.create({
-  iQYDmFzV: {
+  BLSQpQFL: {
     backgroundColor: "#f0f0f1",
     padding: 10,
     position: "relative",
